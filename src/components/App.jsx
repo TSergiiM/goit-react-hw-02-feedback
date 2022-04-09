bd0123d9e79f05034ece7f6e33a4f084e2d65890
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-09 19:35:14 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,9 @@
+import { SectionTitle } from './SectionTitle/SectionTitle';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <SectionTitle title="Please leave feedback"></SectionTitle>
     </div>
   );
 };
