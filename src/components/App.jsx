12e12c1be5651190f54a068d1792e915ddc6fdf1
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-11 20:23:47 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,24 @@
+import { Component } from 'react';
 import { SectionTitle } from './SectionTitle/SectionTitle';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 
-export const App = () => {
-  return (
-    <div>
-      <SectionTitle title="Please leave feedback"></SectionTitle>
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div>
+        <SectionTitle title="Please leave feedback"></SectionTitle>
+        <FeedbackOptions></FeedbackOptions>
+        <SectionTitle title="Statistics"></SectionTitle>
+        <Statistics></Statistics>
+      </div>
+    );
+  }
+}
