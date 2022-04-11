21ca64f6fd0191ac61dd3ba2bad3aaccf599805d
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-11 20:23:42 +0300

    Create FeedbackOptions.jsx

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,9 @@
+export const FeedbackOptions = () => {
+  return (
+    <div>
+      <button type="button">good</button>
+      <button type="button">neutral</button>
+      <button type="button">bad</button>
+    </div>
+  );
+};
