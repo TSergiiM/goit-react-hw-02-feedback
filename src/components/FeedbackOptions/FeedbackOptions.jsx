f48bda868dc140bf3b87bfc95bdd54da5e0b2bad
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-11 23:05:26 +0300

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,9 +1,15 @@
-export const FeedbackOptions = () => {
+export const FeedbackOptions = ({ onGood, onNeutral, onBad }) => {
   return (
     <div>
-      <button type="button">good</button>
-      <button type="button">neutral</button>
-      <button type="button">bad</button>
+      <button type="button" onClick={onGood}>
+        good
+      </button>
+      <button type="button" onClick={onNeutral}>
+        neutral
+      </button>
+      <button type="button" onClick={onBad}>
+        bad
+      </button>
     </div>
   );
 };
