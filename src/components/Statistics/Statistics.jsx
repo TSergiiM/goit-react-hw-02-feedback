818b351c55a1fbd54054bc05a8fa4581b9b13a4d
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-12 01:27:27 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,17 @@
-export const Statistics = ({ good, neutral, bad }) => {
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  countPositiveFeedbackPercentage,
+}) => {
   return (
     <div>
       <p>Good:{good}</p>
       <p>Neutral:{neutral}</p>
       <p>Bad:{bad}</p>
+      <p>Total:{total}</p>
+      <p>Positive feedback:{countPositiveFeedbackPercentage}%</p>
     </div>
   );
 };
