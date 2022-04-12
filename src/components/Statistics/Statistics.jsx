7bb50d6aa1e3e15e9f84200c32876b97e31da3ce
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-12 23:26:28 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 export const Statistics = ({
   good,
   neutral,
@@ -14,4 +15,12 @@ export const Statistics = ({
       <p>Positive feedback:{positivePercentage}%</p>
     </div>
   );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
