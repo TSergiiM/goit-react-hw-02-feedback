d11b98fcdb9f83a2544ee09388fef49927bd8f5c
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2022-04-12 23:26:35 +0300

    Update Notification.jsx

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,3 +1,8 @@
+import PropTypes from 'prop-types';
 export const Notification = ({ message }) => {
   return <div>{message}</div>;
+};
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
 };
